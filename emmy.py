69a55582c8665b9c42f80b9c00234a0a675e07f4
Bhuvika Garg <97007008+giraffegirl224@users.noreply.github.com>
2023-11-14 17:27:12 +0800

    Add files via upload

---
+++ emmy.py
@@ -0,0 +1,24 @@
+name = input("your name please: ")
+name = name.lower()
+if name == "bhuvika" or name == "emmelyn":
+  print("ur amazing and perfect never change")
+elif name == "kaylee":
+  print("GO TO CHOIR")
+elif name == "farah" or name == "farrah":
+  print("im sorry girl but u should go back to year 2 maths class")
+elif name == "claire":
+  print("ur a furry lol")
+elif name == "qiqi":
+  print("u have -134954018668615846103583195 rizz")
+elif name == "ur mum" or name == "ur mom":
+  print("i hate you")
+elif name == "musa":
+  print("ur literally abusive one day cps will come take ratatouille from you")
+elif name == "danica":
+  print("hey kitten")
+elif name == "laura":
+  print("why do u not check ur emails unless i force u")
+elif name == "justin" or name == "the goofy goober":
+  print("so ik now might not be a good time to tell u but i maybe wrote a fanfiction about you that is very popular im sorry ahahahah in my defence i told u but then u forgot so lol")
+else:
+  print("i probably forgot about u sorry lol")
